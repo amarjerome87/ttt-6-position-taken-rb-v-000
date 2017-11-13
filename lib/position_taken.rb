d9d39633e83e4e939afdc018fb16c1590ea05d08
Amar Chatman <amarjerome@gmail.com>
2017-11-13 19:06:00 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken? (board, index)
-  if board[index] != "  "
-    return false
-  else
+  if board[index] == "  "
     return true
+  elsif board[index] != "  "
+    return false
   end
 end
